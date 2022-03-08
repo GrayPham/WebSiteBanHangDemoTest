56415a816a62dda5651f4718e0a62a069ed4faa3
Gray Pham <hunghtk456@gmail.com>
2022-03-08 23:33:42 +0700

    Chỉnh sửa hoàn thiện trang đăng kí

--- Controllers/SanPhamController.cs
+++ Controllers/SanPhamController.cs
@@ -1,35 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebSiteBanHang.Models;
 namespace WebSiteBanHang.Controllers
 {
-    
     public class SanPhamController : Controller
     {
-        // GET: SanPham
         QuanLyBanHangEntities db = new QuanLyBanHangEntities();
-        public ActionResult SanPham1()
-        {
-            var lstSanPhamMoi = db.SanPhams.Where(n => n.Moi == 1);
-
-            ViewBag.ListSP = lstSanPhamMoi;
-            var lstSanPhamDienThoai = db.SanPhams.Where(n => n.MaLoaiSP == 1 );
-            ViewBag.ListDT = lstSanPhamDienThoai;
-            return View();
-        }
-        public ActionResult SanPham2()
-        {
-            return View();
-        }
+        // GET: SanPham
         [ChildActionOnly]
-        public ActionResult SanPhamPartial()
+        public ActionResult SanPhamStyle1Partial()
         {
-            
-
             return PartialView();
         }
+        [ChildActionOnly]
+        public ActionResult SanPhamStyle2Partial()
+        {
+            return PartialView();
+        }
+        // Xây dựng trang xem chi tiết
+        public ActionResult XemChiTiet(int? id, string tensp)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id && n.DaXoa == false);
+            if(sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+        //Xay dung 1 action load san pham theo mã loại sản phẩm và mã nhà sản phẩm
+        public ActionResult SanPham(int? MaLoaiSP, int? MaNSX, string TenNSX)
+        {
+            if (MaLoaiSP == null || MaNSX == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var lstSP = db.SanPhams.Where(n => n.MaLoaiSP == MaLoaiSP && n.MaNSX== MaNSX);
+            if (lstSP.Count() == 0)
+            {
+                return HttpNotFound();
+            }
+            return View(lstSP);
+        }
+
     }
 }
